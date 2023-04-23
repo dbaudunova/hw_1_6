974bff14cfd6f26b801f130be71964ba30e34987
Dilnaz27 <dbaudunova@gmail.com>
2023-04-23 23:28:13 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/hw_1_6/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.geektech.hw_1_6
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.geektech.hw_1_6.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val activityLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                binding.etWords.setText(result.data?.getStringExtra(KEY_FOR_TEXT))
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.btnNext.setOnClickListener {
+            if (binding.etWords.text.isEmpty()) {
+                Toast.makeText(this, "Field cannot be empty", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_FOR_TEXT, binding.etWords.text.toString())
+                activityLauncher.launch(intent)
+            }
+        }
+    }
+
+    companion object {
+        const val KEY_FOR_TEXT = "text_key"
+    }
+}
---
+++ app/src/main/java/com/geektech/hw_1_6/ResultActivity.kt
@@ -0,0 +1,27 @@
+package com.geektech.hw_1_6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geektech.hw_1_6.MainActivity.Companion.KEY_FOR_TEXT
+import com.geektech.hw_1_6.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.etWords.setText(intent.getStringExtra(KEY_FOR_TEXT))
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.btnPrevious.setOnClickListener {
+            intent.putExtra(KEY_FOR_TEXT, binding.etWords.text.toString())
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+    }
+}
